092e41065ffc3502c5a712cd8a7c8e34a47fe50f
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2017-09-03 18:02:22 -0300

    Add files via upload

--- httpborder.js
+++ httpborder.js
@@ -7,18 +7,20 @@
     div.style.left = '0' ;
     div.style.top = '0' ;
     div.style.background = "transparent";
-    div.style.backgroundImage="images/pagewarning.png";
+    div.style.backgroundImage = "url('" + browser.extension.getURL("images/pagewarning.png") + "')";
     document.body.appendChild(div);
     
+    
+     
     var btn = document.createElement("button");
     var btnimg = document.createElement("buttonimg");
     btnimg.style.position = "fixed";
     btnimg.style.left = '0' ;
     btnimg.style.top = '0' ;
-    btnimg.style.width= "25px";
-    btnimg.style.height= "25px";
+    btnimg.style.width= "30px";
+    btnimg.style.height= "30px";
     btnimg.style.color= "transparent";
-    btnimg.style.backgroundImage= "images/closebutton.png";
+    btnimg.style.backgroundImage = "url('" + browser.extension.getURL("images/closebutton.png") + "')";
     btnimg.appendChild(btn);                                
     document.body.appendChild(btnimg);
     btn.style.width= "inherit";
